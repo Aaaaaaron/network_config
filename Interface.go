09ba29bb6081b2eee1feb6eef9b83e8adba9f9bb
aron <245915794@qq.com>
2017-04-21 15:20:09 +0800

    init

--- Interface.go
+++ Interface.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
 
 	log "github.com/Sirupsen/logrus"
@@ -17,48 +16,76 @@ func init() {
 
 type Config struct {
 	HostId  string
-	Bridges []Bridge
+	Devices []Device
 	Bonds   []Bond
+	Bridges []Bridge
 	Vlans   []Vlan
 	//后期想到上面新的配置项可以加在这里
 }
 
-type Bridge struct {
-	Name string
-	Dev  string
-	Addr []net.IPNet
-	Mtu  int
-	Stp  string
+type Device struct {
+	Index int
+	Name  string
+	Addr  []netlink.Addr
 }
 
 type Bond struct {
-	Name string
-	Mode string
-	Dev  string
-	Addr []net.IPNet
+	Index int
+	Name  string
+	Mode  netlink.BondMode
+	Dev   string
+	Addr  []netlink.Addr
+}
+
+type Bridge struct {
+	Index int
+	Name  string
+	Dev   string
+	Addr  []netlink.Addr
+	Mtu   int
+	Stp   string
 }
 
 type Vlan struct {
+	Index  int
 	Name   string
 	Tag    string
 	Parent string
-	Addr   []net.IPNet
+	Addr   []netlink.Addr
 }
 
 func main() {
-	GetSysConfig()
+	config := GetSysConfig()
+	fmt.Println(config)
 }
 
 //func GetSysConfig() Config {
 func GetSysConfig() Config {
+	var config Config
 	links := getLinkList()
 	for _, link := range links {
-		//link.Type()
-		fmt.Println(link)
+		switch link.Type() {
+		case "device":
+			if deviceLink, ok := link.(*netlink.Device); ok {
+				addr, _ := netlink.AddrList(link, netlink.FAMILY_ALL)
+				config.Devices = append(config.Devices, Device{deviceLink.Index, deviceLink.Name, addr})
+			}
+			//case "bond":
+			//	var bond Bond
+			//	if bondLink, ok := link.(*netlink.Bond); ok {
+			//		bond.Index = bondLink.Index
+			//		bond.Name = bondLink.Name
+			//		bond.Mode = bondLink.Mode
+			//		bond.
+			//	}
+			//case "vlan":
+			//case "bridge":
+		}
 	}
+	return config
 }
 
-func getLinkList() []netlink.Link {
+func getLinkList() []netlink.Link { // link represent all network interface
 	linkList, err := netlink.LinkList()
 	if err != nil {
 		log.Fatalf("get link list from netlink failed: %s", err)
