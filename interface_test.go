88b1cfcd383bf85690cad35d598d2d655122d8a5
aron <245915794@qq.com>
2017-04-24 15:18:09 +0800

    init

--- interface_test.go
+++ interface_test.go
@@ -21,6 +21,51 @@ func initNetwork() {
 			}
 		}
 	}
+	addBond("bond0", []string{"eth0"})
+	addBridge("br0", []string{"eth1"})
+	addVlan("eth2.100","eht2",100)
+}
+
+func addBond(name string, dev []string) error {
+	link := netlink.NewLinkBond(netlink.LinkAttrs{Name: name})
+	if err := netlink.LinkAdd(link); err != nil {
+		log.Fatal(err)
+		return err
+	}
+	for _, name := range dev {
+		dev, _ := netlink.LinkByName(name)
+		if err := netlink.LinkSetMasterByIndex(link, dev.Attrs().Index); err != nil {
+			log.Fatal(err)
+			return err
+		}
+	}
+	return nil
+}
+
+func addBridge(name string, dev []string) error {
+	link := &netlink.Bridge{netlink.LinkAttrs{Name: "foo", MTU: 1400}}
+	if err := netlink.LinkAdd(link); err != nil {
+		log.Fatal(err)
+		return err
+	}
+	for _, name := range dev {
+		dev, _ := netlink.LinkByName(name)
+		if err := netlink.LinkSetMasterByIndex(link, dev.Attrs().Index); err != nil {
+			log.Fatal(err)
+			return err
+		}
+	}
+	return nil
+}
+
+func addVlan(name string, parent string, id int) error {
+	par, _ := netlink.LinkByName(parent)
+	link := &netlink.Vlan{netlink.LinkAttrs{Name: "bar", ParentIndex: par.Attrs().Index}, id}
+	if err := netlink.LinkAdd(link); err != nil {
+		log.Fatal(err)
+		return err
+	}
+	return nil
 }
 
 func TestGetDevMap(t *testing.T) {
